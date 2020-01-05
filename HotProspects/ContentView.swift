e2ba61df4a3479dc16325136111701c41177629c
Sam McCullough <samanuel.mc@gmail.com>
2020-01-04 22:40:59 -0800

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Sam McCullough on 2020-01-04.
+//  Copyright © 2020 Sam McCullough. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
